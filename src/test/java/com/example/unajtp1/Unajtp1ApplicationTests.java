8f47c09e02e7d1635920eed6e3226acafa99e245
lmonzon <lmonzon@wecover.co>
2024-09-09 12:01:54 -0300

    se sube primer commit

---
+++ src/test/java/com/example/unajtp1/Unajtp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unajtp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unajtp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
